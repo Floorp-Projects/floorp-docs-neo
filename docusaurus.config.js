5388fc46633336332c4561c59ad08787fa3a20e5
regularentropy <roottotalhax@gmail.com>
2024-07-07 14:20:44 +0200

    docs: add base

---
+++ docusaurus.config.js
@@ -0,0 +1,125 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Floorp Docs',
+  tagline: 'Documentation for the Floorp Browser',
+  favicon: 'img/favicon.png',
+
+  // Set the production url of your site here
+  url: 'https://docs.floorp.app',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['ja', 'en', 'ru', 'uk', 'hu'],
+    path: 'i18n',
+    localeConfigs: {
+      ja: {
+        label: "日本語",
+      },
+      en: {
+        label: "English",
+      },
+      ru: {
+        label: "Русский",
+      },
+      uk: {
+        label: "Українська",
+      },
+      hu: {
+        label: "Magyar",
+      },
+    }
+  },
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+              'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        theme: {
+          customCss: ['./src/css/custom.css'],
+        }
+      }),
+    ],
+  ],
+
+  themeConfig:
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+          title: 'Floorp Docs',
+          logo: {
+            alt: 'Floorp Logo',
+            src: 'img/favicon.png',
+          },
+          items: [
+            {
+              href: 'https://discord.com/invite/f8NFWjx6Rt',
+              position: 'right',
+              className: "fa-brands fa-discord",
+              alt: 'Discord'
+            },
+            {
+              href: 'https://github.com/Floorp-Projects/Floorp',
+              position: 'right',
+              className: "fa-brands fa-github",
+              alt: 'Github'
+            },
+            {
+              type: "localeDropdown", position: "right", queryString: '?persistLocale=true',
+            }
+          ],
+        },
+        // footer: {
+        //   style: 'dark',
+        //   links: [
+        //       {
+        //         label: 'Discord',
+        //         href: 'https://discord.com/invite/f8NFWjx6Rt',
+        //         image: 'img/favicon.png',
+        //       },
+        //       {
+        //       label: 'X',
+        //           href: 'https://twitter.com/floorp_browser',
+        //       },
+        //   ],
+        //   copyright: `Copyright © ${new Date().getFullYear()} Ablaze. Built with Docusaurus.`,
+        // },
+        prism: {
+          theme: prismThemes.github,
+          darkTheme: prismThemes.dracula,
+        },
+      }),
+};
+
+export default config;
---
+++ src/pages/index.js
@@ -0,0 +1,42 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+import FloorpLogo from "/static/img/favicon.png"
+import Link from "@docusaurus/core/lib/client/exports/Link";
+
+import Translate, {translate} from '@docusaurus/Translate';
+
+function DocsLabels() {
+    return (
+        <div className="sp_container">
+            <div className="left-section">
+                <h1>
+                    <Translate>Floorp Docs</Translate>
+                </h1>
+                <p><Translate>Do you have any questions?</Translate></p>
+                <div className="buttons">
+                    <Link className="get-started" to="/docs">
+                        <Translate>Get Started</Translate>
+                    </Link>
+                    <Link className="open-website" to='https://floorp.app'>
+                        <Translate>Floorp Website</Translate>
+                    </Link>
+                </div>
+            </div>
+            <div className="right-section">
+                <img src={FloorpLogo} alt="Floorp Logo" className="logo" />
+                <h1>
+                    Floorp
+                </h1>
+            </div>
+        </div>
+    );
+}
+
+export default function Home() {
+    return (
+        <Layout noFooter={true}>
+            < DocsLabels />
+        </Layout>
+    );
+}

07cb7e6a6a3ce4e925c8bdc9bcbc47f36c277c65
regularentropy <roottotalhax@gmail.com>
2024-07-12 10:48:28 +0200

    docs: temp deployment change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://docs.floorp.app',
+  url: 'https://regularentropy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/floorp-docs-ref/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

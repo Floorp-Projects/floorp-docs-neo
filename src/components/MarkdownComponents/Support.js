89571f73d13ec72259ba317b1f91b93dfb4ad2e0
regularentropy <roottotalhax@gmail.com>
2025-05-02 10:58:55 +0200

    components | support: remove mentions of ablaze to reduce load

--- src/components/MarkdownComponents/Support.js
+++ src/components/MarkdownComponents/Support.js
@@ -8,11 +8,9 @@ const Support = () => {
             <h2><Translate>Need Support?</Translate></h2>
             <ul>
                 <li><Link to="https://discord.floorp.app"><Translate>Discord (Floorp Projects)</Translate></Link></li>
-                <li><Link to="https://x.com/Ablaze_MIRAI">X (@Ablaze_MIRAI)</Link></li>
                 <li><Link to="https://x.com/Floorp_Browser">X (@Floorp_Browser)</Link></li>
                 <li><Link to="https://reddit.com/r/Floorp">Reddit (r/Floorp)</Link></li>
                 <li><Link to="https://misskey.io/@floorp@misskey.io">Misskey.io (@floorp@misskey.io)</Link></li>
-                <li><Link to="https://support.ablaze.one/contact"><Translate>Contact Us</Translate></Link></li>
             </ul>
         </div>
     );
